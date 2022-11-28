f40b2b2e911233e3b8bbb568fb96df2663533300
FilipZobic <zobicfilip919@gmail.com>
2022-11-28 22:02:23 +0100

    Add remote ECR image build functionality & versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.google.cloud.tools.jib") version "3.3.1"
+    id("pl.allegro.tech.build.axion-release") version "1.14.0"
 }
 
 group = "com.m2z.tools"
-version = "0.0.1-SNAPSHOT"
+version = scmVersion.version
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
@@ -45,7 +46,16 @@ extensions.findByName("buildScan")?.withGroovyBuilder {
     setProperty("termsOfServiceAgree", "yes")
 }
 
-// Containerization (Local)
+// Utility method
+fun gitBranch(): String {
+    var branch: String
+    val proc = ProcessBuilder("git", "rev-parse", "--abbrev-ref", "HEAD").start()
+    proc.inputStream.bufferedReader().use { branch = it.readLine()  }
+    proc.waitFor()
+    return branch
+}
+
+// Containerization (Local directly to docker-deamon)
 jib {
     from {
         image = "eclipse-temurin:17-jre"
@@ -64,7 +74,27 @@ tasks.named("jibDockerBuild") {
             to {
                 image = "management-service"
                 credHelper.helper = "osxkeychain"
-                tags = setOf("latest")
+                tags = setOf("latest", gitBranch() + " " + version.toString())
+            }
+        }
+    }
+}
+
+// Containerization (Remote directly to container registry)
+tasks.named("jib") {
+    doFirst {
+        if (!project.hasProperty("remoteRegistry") || !project.hasProperty("remoteImage")) {
+            throw GradleException("Both 'remoteRegistry' & 'remoteImage' variables are required")
+        }
+        val remoteRegistry = project.properties["remoteRegistry"]
+        val remoteImage = project.properties["remoteImage"]
+        jib {
+            to {
+                image = "$remoteRegistry/$remoteImage"
+                if (!project.hasProperty("CI_CD")) {
+                    credHelper.helper = "ecr-login"
+                }
+                tags = setOf("latest"/*, version.toString()*/)
             }
         }
     }
