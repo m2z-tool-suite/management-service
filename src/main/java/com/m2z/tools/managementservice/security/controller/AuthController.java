5993fc695ffe9bf56f08f8f107d61790e47f72ea
FilipZobic <zobicfilip919@gmail.com>
2022-12-10 20:57:55 +0100

    Fix annotations

--- src/main/java/com/m2z/tools/managementservice/security/controller/AuthController.java
+++ src/main/java/com/m2z/tools/managementservice/security/controller/AuthController.java
@@ -4,22 +4,26 @@ import com.m2z.tools.managementservice.generic.GenericResponse;
 import com.m2z.tools.managementservice.security.service.AuthService;
 import com.m2z.tools.managementservice.security.dto.AuthRegistrationDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ControllerAdvice("api/v1/auth/")
+@Controller
+@RequestMapping("api/v1/auth")
 @ResponseBody
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
+    @PostMapping("/signUp")
     public GenericResponse registerUser(@RequestBody AuthRegistrationDTO registrationDTO) {
 
         authService.registerUser(registrationDTO);
 
-        return GenericResponse.ok();
+        return GenericResponse.created("User registered successfully");
     }
 
     public void signInUser() {
