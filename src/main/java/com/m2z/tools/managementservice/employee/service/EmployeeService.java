92e4993119fb3f1270aa8a768b753c6bbcc58239
FilipZobic <zobicfilip919@gmail.com>
2022-12-10 18:39:37 +0100

    Add pagination

--- src/main/java/com/m2z/tools/managementservice/employee/service/EmployeeService.java
+++ src/main/java/com/m2z/tools/managementservice/employee/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.m2z.tools.managementservice.employee.service;
 
 import com.m2z.tools.managementservice.employee.dto.NewEmployeeDTO;
 import com.m2z.tools.managementservice.employee.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface EmployeeService {
 
     Employee createEmployee(NewEmployeeDTO newEmployee);
+
+    Page<Employee> page(Pageable pageable);
 }
--- src/main/java/com/m2z/tools/managementservice/employee/service/RelationalDBEmployeeService.java
+++ src/main/java/com/m2z/tools/managementservice/employee/service/RelationalDBEmployeeService.java
@@ -2,12 +2,19 @@ package com.m2z.tools.managementservice.employee.service;
 
 import com.m2z.tools.managementservice.employee.dto.NewEmployeeDTO;
 import com.m2z.tools.managementservice.employee.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import javax.naming.OperationNotSupportedException;
 
 public class RelationalDBEmployeeService implements EmployeeService {
     @Override
     public Employee createEmployee(NewEmployeeDTO newEmployee) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Page<Employee> page(Pageable pageable) {
         throw new UnsupportedOperationException();
     }
 }
