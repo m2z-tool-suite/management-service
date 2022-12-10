4307099ca3484a66d710c2fb9de24cdf70d21155
FilipZobic <zobicfilip919@gmail.com>
2022-12-10 21:02:11 +0100

    Fix service annotation

--- src/main/java/com/m2z/tools/managementservice/employee/service/RelationalDBEmployeeService.java
+++ src/main/java/com/m2z/tools/managementservice/employee/service/RelationalDBEmployeeService.java
@@ -4,9 +4,9 @@ import com.m2z.tools.managementservice.employee.dto.NewEmployeeDTO;
 import com.m2z.tools.managementservice.employee.model.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-import javax.naming.OperationNotSupportedException;
-
+@Service
 public class RelationalDBEmployeeService implements EmployeeService {
     @Override
     public Employee createEmployee(NewEmployeeDTO newEmployee) {
