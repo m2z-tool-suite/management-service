bae21e09472f66c4c3c1be83c395791333602329
FilipZobic <zobicfilip919@gmail.com>
2022-12-10 12:53:54 +0100

    Add constructors

--- src/main/java/com/m2z/tools/managementservice/employee/dto/NewEmployeeDTO.java
+++ src/main/java/com/m2z/tools/managementservice/employee/dto/NewEmployeeDTO.java
@@ -2,11 +2,15 @@ package com.m2z.tools.managementservice.employee.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewEmployeeDTO {
 
     @NotBlank
--- src/main/java/com/m2z/tools/managementservice/security/dto/AuthRegistrationDTO.java
+++ src/main/java/com/m2z/tools/managementservice/security/dto/AuthRegistrationDTO.java
@@ -1,11 +1,15 @@
 package com.m2z.tools.managementservice.security.dto;
 
 import com.m2z.tools.managementservice.employee.dto.NewEmployeeDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthRegistrationDTO {
     private String email;
     private String password;
