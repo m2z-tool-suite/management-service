1b9bda661be71e993154c7441cbfcc611ecbc909
FilipZobic <zobicfilip919@gmail.com>
2022-12-10 21:01:57 +0100

    Enable property scanning

--- src/main/java/com/m2z/tools/managementservice/ManagementServiceApplication.java
+++ src/main/java/com/m2z/tools/managementservice/ManagementServiceApplication.java
@@ -1,9 +1,12 @@
 package com.m2z.tools.managementservice;
 
+import com.m2z.tools.managementservice.aws.AwsCredentialConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({AwsCredentialConfig.class})
 public class ManagementServiceApplication {
 
     public static void main(String[] args) {
